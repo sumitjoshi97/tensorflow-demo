e5950a2964466d9f1d0977d7de7b4bd5eb655849
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-29 16:30:44 +0530

    chore: add sample data

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('Tensorflow');
+async function getData() {
+  const carsDataReq = await fetch(
+    'https://storage.googleapis.com/tfjs-tutorials/carsData.json'
+  )
+  const carsData = await carsDataReq.json()
+  console.log(carsData)
+  const cleaned = carsData
+    .map(car => ({
+      mpg: car.Miles_per_Gallon,
+      horsePower: car.Horsepower,
+    }))
+    .filter(car => car.mpg !== null && car.horsePower !== null)
+
+  return cleaned
+}
+
+async function run() {
+  const data = await getData()
+  console.log(data)
+  const values = await data.map(d => ({
+    x: d.horsePower,
+    y: d.mpg,
+  }))
+
+  console.log(values)
+  tfvis.render.scatterplot(
+    { name: 'Horsepower v MPG' },
+    { values },
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300,
+    }
+  )
+}
+
+document.addEventListener('DOMContentLoaded', run)
